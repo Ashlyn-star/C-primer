a73af5ac277dd97efdb5423e9f56dcb02ffb5695
Ashlyn-star <214625132@qq.com>
2021-07-21 16:25:02 +0800

    .

---
+++ chapter7/7.27.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<string>
+#include"7.27.h"
+
+using namespace std;
+
+int main() {
+	Screen myScreen(5, 5, 'X');
+	myScreen.move(4, 0).set('#').display(cout);
+	cout << endl;
+	myScreen.display(cout);
+	cout << endl;
+}
--- chapter7/7.27.h
+++ chapter7/7.27.h
@@ -10,22 +10,46 @@ class Screen {
 public:
 	typedef string::size_type pos;
 	Screen() = default;
-	Screen(pos ht, pos wd, int n) :height(ht), width(wd) {}
+	Screen(pos ht, pos wd) :height(ht), width(wd) {}
 	Screen(pos ht, pos wd, char c) :height(ht), width(wd), contents(ht* wd, c) {}
 	char get() const {
 		return contents[cursor];
 	}
 	Screen& move(pos r, pos c);//返回的是那个return*this的screen类对象的引用
+	Screen& set(pos r, pos col, char ch);
+	Screen& set(char ch);
+	Screen& display(ostream& os) {
+		do_display(os);
+		return *this;
+	}
+	const Screen& display(ostream& os) const{
+		do_display(os);
+		return *this;
+	}
 private:
 	pos cursor;
 	pos height = 0, width = 0;
 	string contents;
+	void do_display(ostream& os) const {
+		os << contents;
+	}
 };
 
 inline Screen& Screen::move(pos r, pos c) {
 	pos row = r * width;
 	cursor = row + c;
 	return *this;
+}
+
+inline Screen& Screen::set(char ch) {
+	contents[cursor] = ch;
+	return *this;
+}
+
+inline Screen& Screen::set(pos r,pos col, char ch) {
+	contents[r * width + col] = ch;
+	return *this;
+
 }
 
 #endif
